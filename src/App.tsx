cceeb34d8731228c34e623137c2da5915b0f588a
Ishan Trivedi <astrocoder8046@gmail.com>
2024-03-19 02:29:28 +0530

    :heavy_plus_sign: added react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from "@/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-native-gesture-handler";
 import { MMKV } from "react-native-mmkv";
 import ApplicationNavigator from "./navigators/Application";
 import "./translations";
 
 export const storage = new MMKV();
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider storage={storage}>
-      <ApplicationNavigator />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider storage={storage}>
+        <ApplicationNavigator />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
